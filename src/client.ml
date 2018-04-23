b702fc6d4e36ba7f1dca2c0d48ab665b7300584b
derui <derutakayu@gmail.com>
2018-04-23 17:28:12 +0900

    test: Add test for client

--- src/client.ml
+++ src/client.ml
@@ -2,7 +2,7 @@ open Types
 include Client_intf
 
 (** Make a request and response handler that will use with response which has same id of the request it. *)
-let wrap_request
+let make_request
     (type p) (type r)
     (module A: Api_def with type params = p and type result = r)
     (params: p option)
@@ -17,5 +17,31 @@ let wrap_request
     handler param
   in
   let id = Random.int64 Int64.max_int in
-  let request = jsonrpc_to_json (Request ({id; params; _method = A.name})) in
-  (request, handler')
+  let request = Request ({id; params; _method = A.name}) in
+  (id, request, handler')
+
+(**/**)
+(* ignore document below *)
+
+module Test = struct
+  let tests = [
+    ("should be able to wrap a request with API definition", fun _ ->
+        let module A = struct
+          type params = int list
+          type result = int
+
+          let name = "sum"
+          let params_to_json = function
+            | None -> None
+            | Some v -> Some (`List (List.map (fun v -> `Int v) v))
+          let result_of_json = function
+            | `Int v -> v
+            | _ -> failwith ""
+        end in
+
+        let id, req, handler = make_request (module A) (Some [1;2;3]) ignore in
+        let expected = Request {id = id; params = Some (`List [`Int 1;`Int 2;`Int 3]); _method = "sum"} in
+        expected = req
+    );
+  ]
+end
--- test/test_main.ml
+++ test/test_main.ml
@@ -4,9 +4,12 @@ let () =
   let converter_specs = List.map (fun (desc, spec) ->
       desc >:: (fun _ -> assert_bool "desc" @@ spec ())
     ) Jsonrpc_ocaml.Types.Test.tests
+  and client_specs = List.map (fun (desc, spec) ->
+      desc >:: (fun _ -> assert_bool "desc" @@ spec ())
+    ) Jsonrpc_ocaml.Client.Test.tests
   in
   let suite =
-    "JSON RPC for OCaml" >::: converter_specs
+    "JSON RPC for OCaml" >::: (converter_specs @ client_specs)
   in
 
   run_test_tt_main suite
