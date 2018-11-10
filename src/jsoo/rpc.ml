863762118c42dba25abcc6dd3c651ece029933fb
derui <derutakayu@gmail.com>
2018-11-11 08:24:57 +0900

    feat: add interface for js_of_ocaml

---
+++ src/jsoo/rpc.ml
@@ -0,0 +1,6 @@
+(** Specialized module interface with Js_of_ocaml  *)
+module type S =
+  Jsonrpc_ocaml.Rpc.S
+  with type json = < > Js.t
+   and module Response = Response
+   and module Request = Request
