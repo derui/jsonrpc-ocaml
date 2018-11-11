52a212f47d6d530e68f3a843a0eddb7eaaf0b942
derui <derutakayu@gmail.com>
2018-11-11 10:10:09 +0900

    fix: fix signatures of client

--- src/jsoo/client.ml
+++ src/jsoo/client.ml
@@ -4,10 +4,11 @@ module type Api_def = sig
   include J.Client.Api_def with type json = < > Js.t
 end
 
-module type S = J.Client.S with type json = < > Js.t
+module type S = J.Client.S with type json := < > Js.t
 
-module Make (R : J.Rpc.S with type json = < > Js.t) =
+module Make (R : J.Rpc.S with type json = < > Js.t) :
+  S with module Thread := R.Thread =
   J.Client.Make (struct
-      type t = < > Js.t
-    end)
+    type t = < > Js.t
+  end)
     (R)
--- src/yojson/client.ml
+++ src/yojson/client.ml
@@ -4,10 +4,11 @@ module type Api_def = sig
   include J.Client.Api_def with type json = Yojson.Safe.json
 end
 
-module type S = J.Client.S with type json = Yojson.Safe.json
+module type S = J.Client.S with type json := Yojson.Safe.json
 
-module Make (R : J.Rpc.S with type json = Yojson.Safe.json) =
+module Make (R : J.Rpc.S with type json = Yojson.Safe.json) :
+  S with module Thread := R.Thread =
   J.Client.Make (struct
-      type t = Yojson.Safe.json
-    end)
+    type t = Yojson.Safe.json
+  end)
     (R)
