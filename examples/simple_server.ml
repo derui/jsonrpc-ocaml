6db32f36bbdfe3a7b00b6134b155cdce8dc0e768
derui <derutakayu@gmail.com>
2019-05-09 16:40:20 +0900

    feat: add example server implementation

---
+++ examples/simple_server.ml
@@ -0,0 +1,40 @@
+module Y = Jsonrpc_ocaml_yojson
+
+let jsonrpc_server =
+  let server = Y.Server.make () in
+  Y.Server.expose ~_method:"sample"
+    ~handler:(fun req -> Lwt.return Y.Response.{empty with id = req.id})
+    server
+
+let handler _ (req : Cohttp_lwt_unix.Request.t) (body : Cohttp_lwt.Body.t) =
+  let uri = Cohttp.Request.uri req in
+  match Uri.path uri with
+  | "/" ->
+      let%lwt json = Cohttp_lwt.Body.to_string body in
+      Printf.printf "%s\n%!" json ;
+      let request =
+        match Yojson.Safe.from_string json |> Y.Request.of_json with
+        | Ok v -> v
+        | Error _ -> failwith "fail"
+      in
+      let%lwt res = Y.Server.handle_request ~request jsonrpc_server in
+      let%lwt resp =
+        Cohttp_lwt_unix.Server.respond_string ~status:`OK
+          ~body:(Y.Response.to_json res |> Yojson.Safe.to_string)
+          ()
+      in
+      Lwt.return (`Response resp)
+  | _ ->
+      let%lwt resp =
+        Cohttp_lwt_unix.Server.respond_string ~status:`Not_found
+          ~body:(Sexplib.Sexp.to_string_hum (Cohttp.Request.sexp_of_t req))
+          ()
+      in
+      Lwt.return (`Response resp)
+
+let () =
+  let port = 8081 in
+  Lwt_main.run
+  @@ Cohttp_lwt_unix.Server.create
+       ~mode:(`TCP (`Port port))
+       (Cohttp_lwt_unix.Server.make_response_action ~callback:handler ())
---
+++ readme.md
@@ -0,0 +1,20 @@
+# Minimal JSON-RPC implementation for OCaml #
+
+## build ##
+
+```
+$ dune build
+```
+
+## Run test ##
+
+```
+$ dune runtest
+```
+
+## Build example ##
+jsonrpc-ocaml contains example server implementation that is used with cohttp.
+
+```
+$ dune build @examples
+```
